e8f072f4b1ba2d2087de1fc126efa9f5742e12ab
Alex Owens <emilyowens@Alexs-MacBook-Pro.local>
2022-12-15 16:46:26 -0700

    adds choice of output location

--- MLBTakeHome/main.go
+++ MLBTakeHome/main.go
@@ -9,12 +9,12 @@ import (
 )
 
 // main takes in inputs for date and teamId
-// After calling sorting function, main then writes the newly sorted JSON to
-// the file schedule.go
-// Currently drops returned http errors, and will simply return an empty file
+// After calling sorting function, main either writes the newly sorted JSON to stdout or writes it to
+// the file 'schedule.go'
 func main() {
 	dateFlag := flag.String("date", "2016-11-02", "date in format YYYY-MM-DD")
 	teamFlag := flag.Int("teamId", 112, "Id of favorite team, e.g. 112 for CHC")
+	asFileFlag := flag.Bool("asFile", false, "if this flag is set to true then the return value will be written to the file 'schedule.json'")
 
 	flag.Parse()
 
@@ -27,12 +27,14 @@ func main() {
 		}
 	}
 
-	fmt.Printf("TeamId: %v\n", *teamFlag)
-	fmt.Printf("Date: %v\n", *dateFlag)
-	fmt.Println("Generating sorted schedule...")
-	err = os.WriteFile("schedule.json", resp, 0666)
-	if err != nil {
-		log.Fatal(err)
+	if *asFileFlag {
+		fmt.Println("Generating sorted schedule...")
+		err = os.WriteFile("schedule.json", resp, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Generated schedule in 'schedule.json'")
+	} else {
+		fmt.Println(string(resp))
 	}
-	fmt.Println("Generated schedule in 'schedule.json'")
 }
